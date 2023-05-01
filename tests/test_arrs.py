d7cc3dac46397b3508652cbaf87100fc1f0bb2d2
Margarita Rastriga <rasm@1Ci.com>
2023-05-02 02:27:42 +0300

    issue1 bugfix of tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,11 +5,8 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
-
-def test_get():
     with pytest.raises(IndexError):
         assert arrs.get([], 0)
-
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
