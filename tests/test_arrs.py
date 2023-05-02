6e37b0553031cad73055862a0b8cacf767aba13d
Margarita Rastriga <rasm@1Ci.com>
2023-05-02 12:19:01 +0300

    issue2 new tests and fixture

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,12 +2,27 @@ import pytest
 
 from utils import arrs
 
+@pytest.fixture
+def test_data_get():
+    return [[[1, 2, 3], 1, "test", 2],
+           [[1, 2, 3], -1, "test", "test"],
+           [[1, 2, 3], 1, "test", 2]]
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
+@pytest.fixture
+def test_data_slice():
+    return [[[1, 2, 3, 4], 1, 3, [2, 3]],
+            [[1, 2, 3], 1, None, [2, 3]],
+            [[1, 2, 3], -115, None, [1, 2, 3]]]
+
+def test_get(test_data_get):
+    for data_item in test_data_get:
+        assert arrs.get(data_item[0], data_item[1], data_item[2]) == data_item[3]
     with pytest.raises(IndexError):
         assert arrs.get([], 0)
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+
+def test_slice(test_data_slice):
+    for data_item in test_data_slice:
+        assert arrs.my_slice(data_item[0], data_item[1], data_item[2]) == data_item[3]
+    with pytest.raises(AssertionError):
+        assert arrs.my_slice([], 8)
